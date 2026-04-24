7125c43818002af97da3653b205b6d8e57faac30
Robbyx19 <Robbyrubio19@gmail.com>
2026-04-24 19:39:07 +0000

    subia

---
+++ js-events/script.js
@@ -0,0 +1,11 @@
+function articles1() {
+    alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.")
+}
+
+function articles2() {
+    alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.")
+}
+
+function articles3() {
+    alert ("We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.")
+}
---
+++ js-variables/script.js
@@ -0,0 +1,25 @@
+function function1() {
+    const name = "Alex";
+    const color = "Gray";
+    let loc = "Home";
+    let mood = "Godis good ";
+    console.log(`${name}, ${color}, ${loc}, ${mood}`);
+}
+
+function function2() {
+    const companyName = "Messi is the Goat";
+    const foundingYear = 2023 ;
+    let isActive = true;
+    let fundingAmount;
+    console.log(`${companyName}, ${foundingYear}, ${isActive}, ${fundingAmount}`);
+}
+
+function function3() {
+    let num = 10;
+    let x = num;
+    x = x * 2;
+    x = x + 10;
+    x = x / 2;
+    x = x - num;
+    alert("The value is " + x);
+}

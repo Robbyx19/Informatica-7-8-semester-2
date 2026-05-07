3834ccc8740011d970d0146978ce593488b0145e
Robbyx19 <Robbyrubio19@gmail.com>
2026-05-07 17:30:33 +0000

    calahorra skibidi

--- greeting/script.js
+++ greeting/script.js
@@ -3,9 +3,20 @@ document.querySelector("form").onsubmit = function(){
     let name =document.querySelector("#name").value;
     console.log("Hi"+ name);
 
-    document.querySelector("h1").innerHTML = "Hi" + name;
+    document.querySelector("h1").innerHTML = "Hi " + name;
 
+    let now = new Date();
+    console.log(now.getHours());
+
+    let hour = now.getHours();
+    if (hour < 12 ){
+        document.querySelector("#time").innerHTML="😁Good Morning🌅";
+    } else if (hour < 18){
+     document.querySelector("#time").innerHTML = "Good Afternoon!";
+ }else {
+    document.querySelector("#time").innerHTML =" good night fella!🌛 "; }
     return false;
+
 
 }
  });

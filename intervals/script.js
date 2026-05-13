25d339085ebcfea9090f65233962be2d9b8cca2f
Robbyx19 <Robbyrubio19@gmail.com>
2026-05-13 20:22:20 +0000

    calahorra me enseñoa ahcer esto

--- intervals/script.js
+++ intervals/script.js
@@ -1,24 +1,43 @@
-const  alarma = new Audio ("sound/alarma.mp3");
-let counter = "00:01" ;
+const kick = new Audio("sound/alarma.mp3");
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#one').onclick = function(){
+        counter = 60;
+        document.querySelector('h1').innerHTML = "1:00";
+    }
+        document.querySelector('#five').onclick = function(){
+        counter = 300;
+        document.querySelector('h1').innerHTML = "5:00";
+    }
+        document.querySelector('#ten').onclick = function(){
+        counter = 600;
+        document.querySelector('h1').innerHTML = "10:00";
+    }
+});
+
 
 function count() {
-   if (counter > 0){
- counter--;
- console.log(counter);
-document.querySelector('h1').innerHTML = counter;
-
-}
-else if (counter== 0){
-    counter--;
-    alarma.currentTime = 0;
-    alarma.play();
-}
-}
-
-    document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = function(){
-        setInterval(count, 1000);
-        document.querySelector("button").disabled=true;
-
+    if(counter > 0) {
+        counter--;
+        let mins = Math.floor(counter/60);
+        let seconds = counter-(mins*60);
+        if (seconds < 10) {
+            seconds = "0"+seconds;
         }
+        document.querySelector('h1').innerHTML = mins + ":" + seconds;
+    }
+    if(counter == 0) {
+        alarm.currentTime = 0;
+        alarm.play();
+        counter--;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#start').onclick = function(){
+        setInterval(count, 1000);
+        document.querySelector("#start").disabled = true;
+        document.querySelector("#one").disabled = true;
+        document.querySelector("#five").disabled = true;
+        document.querySelector("#ten").disabled = true;
+    }
 });
